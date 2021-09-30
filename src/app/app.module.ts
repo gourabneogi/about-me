6747940ba6f433041ec91c7dc22e29d61a7d84de
Gourab Neogi <gourabneogi25@gmail.com>
2021-09-30 21:39:40 +0530

    carousal jumbotron added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CarousalComponent } from './carousal/carousal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    JumbotronComponent,
+    CarousalComponent
   ],
   imports: [
     BrowserModule,
